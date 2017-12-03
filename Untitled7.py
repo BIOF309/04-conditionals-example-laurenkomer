0d35250e97d02f22004505728f3e782590f600f2
laurenkomer <31864369+laurenkomer@users.noreply.github.com>
2017-12-03 14:37:27 -0500

    Add files via upload

---
+++ Untitled7.py
@@ -0,0 +1,18 @@
+
+# coding: utf-8
+
+# In[1]:
+
+room_areas = [12, 13, 18, 20, 9]
+
+for i in room_areas:
+  if i > 10:
+    print("Very small!")
+  else:
+    print("Very big!")
+
+
+# In[ ]:
+
+
+
